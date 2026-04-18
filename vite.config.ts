9da181aea77203074e8031324db6f372db442016
lozzoljuh <randomyeslucks@gmail.com>
2026-04-17 17:19:10 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/classy-construction-website-design/',
+  base: '/eastvale/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
